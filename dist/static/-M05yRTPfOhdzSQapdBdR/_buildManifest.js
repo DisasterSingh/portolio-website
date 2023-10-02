23583fcd4630d36645b8e242a89686a73f9b5ed4
Devesh Singh Verma <deveshsinghverma@Deveshs-Air.bbrouter>
2023-10-02 12:10:28 +0530

    Fixed Mobile Number and Email Issue

--- dist/static/-M05yRTPfOhdzSQapdBdR/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/653-87ca0671d142822b.js","static/chunks/pages/index-0d56948addfb93ae.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
